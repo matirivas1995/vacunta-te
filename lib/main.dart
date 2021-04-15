ce660e2da814f88e5490ad6892a7ed92432e9d0d
RUIZ DIAZ FABIO Jose Ariel <ariel.ruiz@tigo.net.py>
2021-04-15 13:19:57 -0400

    aproximacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'src/locations.dart' as locations;
 import 'package:location/location.dart';
 
@@ -45,19 +46,48 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: SafeArea(
+      child: Scaffold(
         appBar: AppBar(
           title: const Text('Centros de Vacunacion', style: TextStyle(color: Colors.white, fontSize: 28),),
           backgroundColor: Color(0xff009AAD),
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(target: _initialcameraposition, zoom: 13),
-          markers: _markers.values.toSet(),
-          myLocationEnabled: true,
-          myLocationButtonEnabled: true,
+        body: Stack(
+          children: [
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(target: _initialcameraposition, zoom: 13),
+              markers: _markers.values.toSet(),
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+            ),
+
+            SlidingUpPanel(
+                minHeight: 50,
+                maxHeight: 400,
+                panel: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        height: 4,
+                        width: 200,
+                        color: Colors.orange,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ]
+                  ),
+                ),
+            ),
+
+          ]
+          ),
         ),
       ),
     );
