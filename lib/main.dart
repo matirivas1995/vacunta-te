641c7befede85c427885574927f6e7e48c633277
RUIZ DIAZ FABIO Jose Ariel <ariel.ruiz@tigo.net.py>
2021-04-14 21:09:11 -0400

    vacunta-te

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'src/locations.dart' as locations;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getCentrosVacunatorios();
+    setState(() {
+      _markers.clear();
+      for (final centro in googleOffices.centros) {
+        final marker = Marker(
+          markerId: MarkerId(centro.name),
+          position: LatLng(centro.lat, centro.lng),
+          infoWindow: InfoWindow(
+            title: centro.name,
+            snippet: centro.address,
+          ),
+        );
+        _markers[centro.name] = marker;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Centros vacunatorios'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: const LatLng(0, 0),
+            zoom: 2,
+          ),
+          markers: _markers.values.toSet(),
+        ),
+      ),
+    );
+  }
+}
