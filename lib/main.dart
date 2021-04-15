1defb38ba2f4ec249fe231f25d1147553b887caf
RUIZ DIAZ FABIO Jose Ariel <ariel.ruiz@tigo.net.py>
2021-04-15 16:06:36 -0400

    no funciona aun

--- lib/main.dart
+++ lib/main.dart
@@ -46,50 +46,72 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Centros de Vacunacion', style: TextStyle(color: Colors.white, fontSize: 28),),
-          backgroundColor: Color(0xff009AAD),
-        ),
-        body: Stack(
-          children: [
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Centros de Vacunacion',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+            backgroundColor: Color(0xff009AAD),
+          ),
+          body: Stack(children: [
             GoogleMap(
               onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(target: _initialcameraposition, zoom: 13),
+              initialCameraPosition:
+                  CameraPosition(target: _initialcameraposition, zoom: 13),
               markers: _markers.values.toSet(),
               myLocationEnabled: true,
               myLocationButtonEnabled: true,
             ),
-
             SlidingUpPanel(
-                minHeight: 50,
-                maxHeight: 400,
-                panel: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        height: 4,
-                        width: 200,
-                        color: Colors.orange,
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                    ]
+              minHeight: 50,
+              maxHeight: 400,
+              panelBuilder: (ScrollController sc) => _scrollingList(sc),
+              
+              collapsed: Container(
+                color: Colors.blueGrey,
+                child: Center(
+                  child: Text(
+                    "Ver Centros mas cercanos",
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
+              ),
             ),
-
-          ]
-          ),
+          ]),
         ),
       ),
     );
+  }
+
+
+  Widget _scrollingList(ScrollController sc) {
+    
+//final centro in googleOffices.centros
+
+    return FutureBuilder(
+    builder: (context, offices) {
+      if (offices.connectionState == ConnectionState.none &&
+          offices.hasData == null) {
+        //print('project snapshot data is: ${offices.data}');
+        return Container();
+      }
+      return ListView.builder(
+        itemCount: offices.data.centros.length,
+        itemBuilder: (context, index) {
+          Office project = offices.data.centros[index];
+          return Column(
+            children: <Widget>[
+              // Widget to display the list of project
+            ],
+          );
+        },
+      );
+    },
+    future: locations.getCentrosVacunatorios(),
+  );
   }
 }
